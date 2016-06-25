a10c2dd83ea6e981f6e828f66faf6695a9bb9b45
Andre Aubin <andre@orga.42.fr>
2016-06-25 15:33:46 +0200

    rename to apiize

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# apify [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url] [![Coverage percentage][coveralls-image]][coveralls-url]
+# apiize [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url] [![Coverage percentage][coveralls-image]][coveralls-url]
 > A testing tool for bots interactions
 
 **Turn any json file into a full, explorable, REST API.**
@@ -6,25 +6,25 @@
 ## Install
 
 ```sh
-$ npm install -g apify
+$ npm install -g apiize
 ```
 
 
 ## Command line usage
 
-```js
-var apify = require('apify');
+```bash
+var apiize = require('apiize');
 
-apify('Rainbow');
+apiize('Rainbow');
 ```
 
 
 ## Usage
 
 ```js
-var apify = require('apify');
+var apiize = require('apiize');
 
-apify('Rainbow');
+apiize('Rainbow');
 ```
 
 ## License
@@ -32,11 +32,11 @@ apify('Rainbow');
 MIT © [Andre Aubin](http://andral.kiwi)
 
 
-[npm-image]: https://badge.fury.io/js/apify.svg
-[npm-url]: https://npmjs.org/package/apify
-[travis-image]: https://travis-ci.org/lambda2/apify.svg?branch=master
-[travis-url]: https://travis-ci.org/lambda2/apify
-[daviddm-image]: https://david-dm.org/lambda2/apify.svg?theme=shields.io
-[daviddm-url]: https://david-dm.org/lambda2/apify
-[coveralls-image]: https://coveralls.io/repos/github/lambda2/apify/badge.svg?branch=master
-[coveralls-url]: https://coveralls.io/github/lambda2/apify?branch=master
+[npm-image]: https://badge.fury.io/js/apiize.svg
+[npm-url]: https://npmjs.org/package/apiize
+[travis-image]: https://travis-ci.org/lambda2/apiize.svg?branch=master
+[travis-url]: https://travis-ci.org/lambda2/apiize
+[daviddm-image]: https://david-dm.org/lambda2/apiize.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/lambda2/apiize
+[coveralls-image]: https://coveralls.io/repos/github/lambda2/apiize/badge.svg?branch=master
+[coveralls-url]: https://coveralls.io/github/lambda2/apiize?branch=master
--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 
 import chai from 'chai';
 import { get } from 'get-content';
-import Apify from '../lib';
+import Apiize from '../lib';
 
 const assert = chai.assert;
 
@@ -10,20 +10,20 @@ const jsonTestFile = './test/datasets/punchlines.json';
 // const jsonTestInexistantFile = './i-dont-exist.json';
 const jsonTestUrl = 'http://opendata.paris.fr/explore/dataset/les-1000-titres-les-plus-reserves-dans-les-bibliotheques-de-pret/download\?format\=json';
 
-describe('apify', function () {
+describe('apiize', function () {
 
   describe('boot', function () {
 
     it('should not be nil', function (done) {
-      let apify = new Apify(jsonTestFile);
-      assert.isOk(apify, 'should not be nil');
+      let apiize = new Apiize(jsonTestFile);
+      assert.isOk(apiize, 'should not be nil');
       done();
     });
 
     it('should be created from file', function (done) {
       // this.timeout(10000);
-      let apify = new Apify(jsonTestFile);
-      apify.on('ready', function (e) {
+      let apiize = new Apiize(jsonTestFile);
+      apiize.on('ready', function (e) {
         assert.isOk(e, 'should not be nil');
         assert.isOk(e.rawData, 'should not be nil');
         done();
@@ -33,14 +33,14 @@ describe('apify', function () {
     // it('should fail on unparsable file', function () {
     //   this.timeout(10000);
     //   assert.throws(() => {
-    //     new Apify(jsonTestBadFile);
+    //     new Apiize(jsonTestBadFile);
     //   }, Error, /content is not parsable/, 'must throw an error');
     // });
 
     it('should be created from url', function (done) {
       this.timeout(5000);
-      let apify = new Apify(jsonTestUrl);
-      apify.on('ready', function (e) {
+      let apiize = new Apiize(jsonTestUrl);
+      apiize.on('ready', function (e) {
         assert.isOk(e, 'should not be nil');
         assert.isOk(e.rawData, 'should not be nil');
         done();
@@ -49,8 +49,8 @@ describe('apify', function () {
 
     it('should create server', function (done) {
       // this.timeout(5000);
-      let apify = new Apify(jsonTestFile);
-      apify.on('ready', function (e) {
+      let apiize = new Apiize(jsonTestFile);
+      apiize.on('ready', function (e) {
         assert.isOk(e, 'should not be nil');
         assert.isOk(e.rawData, 'should not be nil');
         assert.isUndefined(e.server, 'should be nil');
