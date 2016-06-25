f5e08b57bba220bc1244a21e799b5b8e3b288889
Andre Aubin <andre@orga.42.fr>
2016-06-25 17:56:31 +0200

    args

--- dist/cli.js
+++ dist/cli.js
@@ -12,7 +12,7 @@ var Info = require('../package.json');
 // If we are called from the command line
 
 exports['default'] = function () {
-  program.version(Info.version).usage('[options] <file>').option('-p, --port <port>', 'Set the server port (default to 1313)', parseInt).option('-r, --root <root_key>', 'Get only data under the specified root key').option('-v, --verbose', 'Add additional logs').arguments('<file> [options]').action(function (cmd, env, o) {
+  program.version(Info.version).usage('[options] <link>').option('-p, --port <port>', 'Set the server port (default to 1313)', parseInt).option('-r, --root <root_key>', 'Get only data under the specified root key').option('-v, --verbose', 'Add additional logs').arguments('<link> [options]').action(function (cmd, env, o) {
     console.log('Action !', cmd, env, o.verbose);
     var apiize = new Apiize(cmd, o);
     apiize.on('ready', function (e) {
@@ -20,7 +20,7 @@ exports['default'] = function () {
     });
   }).parse(process.argv);
 
-  if (process.argv.length === 2) {
+  if (process.argv.length === 2 || program.args.length === 0) {
     program.help();
   }
 };
